4d4352c8df7426c4066db95fcf7e9321b55814d2
reinalta <reinsugianto1412@gmail.com>
2022-05-03 15:13:39 +0700

    Update automatedTesting.py

--- Program/automatedTesting.py
+++ Program/automatedTesting.py
@@ -10,7 +10,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-driver = webdriver.Chrome(executable_path='D:\Kuliah\Skripsi\Prasyarat\python\chromedriver.exe')
+driver = webdriver.Chrome()
 parser = ConfigParser()
 parser.read('database.config')
 
@@ -19,10 +19,15 @@ while (i <= i):
     x = parser.get('database_config',str(i)).split()
     if  x[0] == "open" :
         driver.get(x[1]) 
+        i += 1    
     elif x[0] == "click":
         driver.find_element(By.CSS_SELECTOR, x[1]).click()
+        i += 1    
     elif x[0] == "sendkeys":
         inpt =  WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR,x[1])))
-        inpt.send_keys(x[2])           
-    i += 1        
-
+        inpt.send_keys(x[2])        
+        i += 1    
+    elif x[0] == "quit":
+        driver.quit()
+        break
+         
